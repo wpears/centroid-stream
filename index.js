ff54e2bf82a1b234743e7f03ae5e0a24b305c956
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-07-16 12:26:00 -0400

    Copy over all props on the original chunk

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var through = require('through2');
 var centroid = require('turf-centroid');
 
 var point = 'Point';
+var type = 'type';
+var geometry = 'geometry';
 
 module.exports = function(){
   return through.obj(function(chunk, enc, cb){
@@ -9,8 +11,17 @@ module.exports = function(){
     var output = {
       type: point,
       geometry: centroid(chunk).geometry,
-      properties: chunk.properties
+      properties: null
     };
+
+    var props = Object.keys(chunk);
+
+    for(var i=0; i<props.length; i++){
+      var key = props[i];
+      if(key !== type && key !== geometry){
+        output[key] = chunk[key];
+      }
+    }
 
     cb(null, output);
   });
